85da4ff3b55d859c96422527b9549b962497f148
pjh <pjh4858@naver.com>
2019-10-28 16:07:47 +0900

    'ddd'

---
+++ Source001.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+	int a;
+	char c = 255;
+//	scanf_s("%d", &a, 4);
+//	printf("%d", a);
+	printf("%d\n", c);
+
+	char ch;
+	char arr[10] = { 'a','b','c' };
+	printf("%d\n", arr[3]);
+	printf("%d\n", *(arr+3));
+
+	return 0;
+}
+\
