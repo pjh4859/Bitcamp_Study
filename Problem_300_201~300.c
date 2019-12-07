9532e451f7e6501d3d91c9b95f9e71f0f43d2d10
JAEHEE PARK <pjh4859@naver.com>
2019-12-07 21:16:00 +0900

    '20191207'

--- Problem_300_201~300.c
+++ Problem_300_201~300.c
@@ -1225,10 +1225,114 @@ void main()
 	printf("UT와 한국 LT시간차이:%g 시간 \n", difftime(n1, n2) / 3600.);
 }
 #endif
-#if 1 //252 5초간 지연하는 함수 구현하기(clock)
+#if 0 //252 5초간 지연하는 함수 구현하기(clock)
+#include<stdio.h>
+#include<time.h>
+void sleep(int sec);
+
+void main()
+{
+	time_t n1, n2;
+
+	time(&n1);
+	sleep(5);
+	time(&n2);
+
+	printf("%g초가 지연되었습니다.\n", difftime(n2, n1));
+
+}
+void sleep(int sec)
+{
+	clock_t ct;
+	ct = clock();
+	while (ct + CLK_TCK * sec > clock());
+}
+#endif
+#if 0 //253 두 시간 간의 차이 구하기(mktime)
 #include<stdio.h>
 #include<time.h>
 
+void main()
+{
+	struct tm t1, t2;
+	int n1, n2, n3;
 
+	t1.tm_year = 2020 - 1900;
+	t1.tm_mon = 6 - 1;
+	t1.tm_mday = 15;
+	t1.tm_hour = 1;
+	t1.tm_min = 12;
+	t1.tm_sec = 50;
+
+	t2.tm_year = 2020 - 1900;
+	t2.tm_mon = 6 - 1;
+	t2.tm_mday = 15;
+	t2.tm_hour = 3;
+	t2.tm_min = 35;
+	t2.tm_sec = 22;
+
+	n1 = mktime(&t1);
+	n2 = mktime(&t2);
+
+	n3 = n2 - n1;
+
+	printf("시간 1:%4d-%02d-%02d %02d:%02d:%02d\n",
+		t1.tm_year + 1900, t1.tm_mon + 1, t1.tm_mday,
+		t1.tm_hour, t1.tm_min, t1.tm_sec);
+
+	printf("시간 2:%4d-%02d-%02d %02d:%02d:%02d\n",
+		t2.tm_year + 1900, t2.tm_mon + 1, t2.tm_mday,
+		t2.tm_hour, t2.tm_min, t2.tm_sec);
+
+	printf("시간차이: %d:%d:%d\n",
+		n3 / 3600, (n3 - ((n3 / 3600) * 3600)) / 60, n3 % 60);
+}
+#endif
+#if 0 //두 날짜 간의 차이 구하기(mktime)
+#include<stdio.h>
+#include<time.h>
+#pragma warning(disable:4996)
+
+void main()
+{
+	struct tm t1, t2, t3;
+	int n1, n2;
+	__int64	n3;
+
+	t1.tm_year = 2020 - 1900;
+	t1.tm_mon = 6 - 1;
+	t1.tm_mday = 23;
+	t1.tm_hour = 1;
+	t1.tm_min = 12;
+	t1.tm_sec = 50;
+
+	t2.tm_year = 2020 - 1900;
+	t2.tm_mon = 8 - 1;
+	t2.tm_mday = 19;
+	t2.tm_hour = 3;
+	t2.tm_min = 35;
+	t2.tm_sec = 22;
+
+	n1 = mktime(&t1);
+	n2 = mktime(&t2);
+
+	n3 = n2 - n1;
+	t3 = *gmtime(&n3);
+	t3.tm_year -= 70;
+
+	printf("시간 1:%4d-%02d-%02d %02d:%02d:%02d\n",
+		t1.tm_year + 1900, t1.tm_mon + 1, t1.tm_mday,
+		t1.tm_hour, t1.tm_min, t1.tm_sec);
+
+	printf("시간 2:%4d-%02d-%02d %02d:%02d:%02d\n",
+		t2.tm_year + 1900, t2.tm_mon + 1, t2.tm_mday,
+		t2.tm_hour, t2.tm_min, t2.tm_sec);
+
+	printf("날짜차이 :%4d-%02d-%02d %02d:%02d:%02d\n",
+		t3.tm_year + 1900, t3.tm_mon + 1, t3.tm_mday,
+		t3.tm_hour, t3.tm_min, t3.tm_sec);
+}
+#endif
+#if 1 //255 올해의 경과된 날짜 수 구하기(localtime)
 
 #endif
